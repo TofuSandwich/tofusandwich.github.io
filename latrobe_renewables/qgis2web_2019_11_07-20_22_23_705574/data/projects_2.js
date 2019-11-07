fe69036bb367bc1b0360bff39125cdd6dea55387
TofuSandwich <46140091+TofuSandwich@users.noreply.github.com>
2019-11-07 20:48:17 +1100

    Latrobe renewables

---
+++ latrobe_renewables/qgis2web_2019_11_07-20_22_23_705574/data/projects_2.js
@@ -0,0 +1,18 @@
+var json_projects_2 = {
+"type": "FeatureCollection",
+"name": "projects_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Name": "Lang Lang", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.564634679449654, -38.266064988020403 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Name": "Wonthaggi", "type": "Solar", "capacity": 92.0 }, "geometry": { "type": "Point", "coordinates": [ 145.591661528271516, -38.609895999598216 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Name": "Wonthaggi 2", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.605221739689483, -38.601396872014654 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Name": "Star of the South", "type": "Wind", "capacity": 2000.0 }, "geometry": { "type": "Point", "coordinates": [ 146.73962753352717, -38.716513235191876 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Name": "Delburn", "type": "Wind", "capacity": 300.0 }, "geometry": { "type": "Point", "coordinates": [ 146.225922237253826, -38.319069267366778 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Name": "Morwell", "type": "Solar", "capacity": 70.0 }, "geometry": { "type": "Point", "coordinates": [ 146.407637625593281, -38.197778114974234 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Name": "Toongabbie", "type": "Solar", "capacity": 75.0 }, "geometry": { "type": "Point", "coordinates": [ 146.621521086444943, -38.05105886283139 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Name": "Licola Wilderness village ", "type": "n\/a", "capacity": null }, "geometry": { "type": "Point", "coordinates": [ 146.621435533060549, -37.628210772308087 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "Name": "Maffra", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 146.983411902205745, -37.964504656202124 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "Name": "Gippsland Solar Park", "type": "Solar", "capacity": 50.0 }, "geometry": { "type": "Point", "coordinates": [ 147.036112786959507, -38.268877747143513 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "Name": "Ramahyuck solar farm", "type": "Solar", "capacity": 4.9 }, "geometry": { "type": "Point", "coordinates": [ 147.223816912202921, -38.164422034012112 ] } }
+]
+}
