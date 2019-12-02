575e72012585db615289e366e84dae8eaf7ed30e
TofuSandwich <46140091+TofuSandwich@users.noreply.github.com>
2019-12-02 11:07:03 +1100

    update 2

--- 20191107_Latrobe_renewables/webmap/qgis2web_2019_11_07-20_22_23_705574/data/projects_2.js
+++ 20191107_Latrobe_renewables/webmap/qgis2web_2019_11_07-20_22_23_705574/data/projects_2.js
@@ -1,18 +1,20 @@
 var json_projects_2 = {
-"type": "FeatureCollection",
-"name": "projects_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "fid": "1", "Name": "Lang Lang", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.564634679449654, -38.266064988020403 ] } },
-{ "type": "Feature", "properties": { "fid": "2", "Name": "Wonthaggi", "type": "Solar", "capacity": 92.0 }, "geometry": { "type": "Point", "coordinates": [ 145.591661528271516, -38.609895999598216 ] } },
-{ "type": "Feature", "properties": { "fid": "3", "Name": "Wonthaggi 2", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.605221739689483, -38.601396872014654 ] } },
-{ "type": "Feature", "properties": { "fid": "4", "Name": "Star of the South", "type": "Wind", "capacity": 2000.0 }, "geometry": { "type": "Point", "coordinates": [ 146.73962753352717, -38.716513235191876 ] } },
-{ "type": "Feature", "properties": { "fid": "5", "Name": "Delburn", "type": "Wind", "capacity": 300.0 }, "geometry": { "type": "Point", "coordinates": [ 146.225922237253826, -38.319069267366778 ] } },
-{ "type": "Feature", "properties": { "fid": "6", "Name": "Morwell", "type": "Solar", "capacity": 70.0 }, "geometry": { "type": "Point", "coordinates": [ 146.407637625593281, -38.197778114974234 ] } },
-{ "type": "Feature", "properties": { "fid": "7", "Name": "Toongabbie", "type": "Solar", "capacity": 75.0 }, "geometry": { "type": "Point", "coordinates": [ 146.621521086444943, -38.05105886283139 ] } },
-{ "type": "Feature", "properties": { "fid": "8", "Name": "Licola Wilderness village ", "type": "n\/a", "capacity": null }, "geometry": { "type": "Point", "coordinates": [ 146.621435533060549, -37.628210772308087 ] } },
-{ "type": "Feature", "properties": { "fid": "9", "Name": "Maffra", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 146.983411902205745, -37.964504656202124 ] } },
-{ "type": "Feature", "properties": { "fid": "10", "Name": "Gippsland Solar Park", "type": "Solar", "capacity": 50.0 }, "geometry": { "type": "Point", "coordinates": [ 147.036112786959507, -38.268877747143513 ] } },
-{ "type": "Feature", "properties": { "fid": "11", "Name": "Ramahyuck solar farm", "type": "Solar", "capacity": 4.9 }, "geometry": { "type": "Point", "coordinates": [ 147.223816912202921, -38.164422034012112 ] } }
-]
-}
+    "type": "FeatureCollection",
+    "name": "projects_2",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "fid": "1", "Name": "Lang Lang Solar Farm", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.564634679449654, -38.266064988020403 ] } },
+    { "type": "Feature", "properties": { "fid": "2", "Name": "Wonthaggi", "type": "Solar", "capacity": 92.0 }, "geometry": { "type": "Point", "coordinates": [ 145.591661528271516, -38.609895999598216 ] } },
+    { "type": "Feature", "properties": { "fid": "3", "Name": "Wonthaggi 2", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 145.605221739689483, -38.601396872014654 ] } },
+    { "type": "Feature", "properties": { "fid": "4", "Name": "Star of the South", "type": "Wind", "capacity": 2000.0, "status": "Proposed - 1st offshore wind farm in Australia. Could power 200,000 homes." }, "geometry": { "type": "Point", "coordinates": [ 146.73962753352717, -38.716513235191876 ] } },
+    { "type": "Feature", "properties": { "fid": "5", "Name": "Delburn Wind Project", "type": "Wind", "capacity": 300.0, "status": "Proposed - 200MW, 35 wind turbines in the HVP plantation " }, "geometry": { "type": "Point", "coordinates": [ 146.250223976443181, -38.317103370702512 ] } },
+    { "type": "Feature", "properties": { "fid": "6", "Name": "SEA Electric Vehicle Factory", "type": "Solar", "capacity": 70.0, "status": "Proposed - Looking for a suitable site in Morwell " }, "geometry": { "type": "Point", "coordinates": [ 146.407637625593281, -38.197778114974234 ] } },
+    { "type": "Feature", "properties": { "fid": "7", "Name": "Frasers Solar Farm", "type": "Solar", "capacity": 75.0, "status": "The project is currently under development and we’re expecting to commence construction in 2020 with full operation in late 2021. The solar farm could provide power to approximately 25,000 homes." }, "geometry": { "type": "Point", "coordinates": [ 146.604681059662994, -38.079899125441614 ] } },
+    { "type": "Feature", "properties": { "fid": "8", "Name": "Licola Wilderness village ", "type": "n\/a", "capacity": null, "status": "Built - Licola is the only town in Victoria not connected to the grid. They have been generating their own power which has included using over $100,000 in diesel each year. " }, "geometry": { "type": "Point", "coordinates": [ 146.621435533060549, -37.628210772308087 ] } },
+    { "type": "Feature", "properties": { "fid": "9", "Name": "Maffra", "type": "Solar", "capacity": 30.0 }, "geometry": { "type": "Point", "coordinates": [ 146.983411902205745, -37.964504656202124 ] } },
+    { "type": "Feature", "properties": { "fid": "10", "Name": "Gippsland Solar Park", "type": "Solar", "capacity": 50.0, "status": "Proposed - Stage 1 is a 255 acre solar farm with a 50MW battery" }, "geometry": { "type": "Point", "coordinates": [ 147.036112786959507, -38.268877747143513 ] } },
+    { "type": "Feature", "properties": { "fid": "11", "Name": "Ramahyuck Solar Farm", "type": "Solar", "capacity": 4.9, "status": "Proposed - A preliminary feasibility study has been completed" }, "geometry": { "type": "Point", "coordinates": [ 147.087539160375002, -38.170763671968459 ] } },
+    { "type": "Feature", "properties": { "fid": "12", "Name": "Alberton Wind Farm", "type": "Wind", "capacity": null, "status": "Proposed - 34 Turbines" }, "geometry": { "type": "Point", "coordinates": [ 146.666528895795807, -38.620759886037789 ] } }
+    ]
+    }
+    
